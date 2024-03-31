16c76ee8e388185a9c2b1b443fa10798f8dac6ec
didar <deddarnawzad@gmail.com>
2024-03-31 22:28:33 +0300

    create movie card component and resuing

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+export default function MovieCard({ movie }) {
+  return (
+    <Link className="flex flex-col border h-96" href={`/movies/${movie.id}`}>
+      <div className="flex-grow ">
+        <Image
+          width={320}
+          height={320}
+          className="object-cover select-none h-80"
+          src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+          alt="Image Description"
+        />
+      </div>
+      <div className="z-10">
+        <h3 className="text-lg font-bold text-gray-800 dark:text-white">
+          {movie.original_title}
+        </h3>
+        <p className="mt-1 text-gray-500 dark:text-gray-400">Some quick</p>
+      </div>
+    </Link>
+  );
+}

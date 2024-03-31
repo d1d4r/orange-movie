74afc32e821ebb8716c3a8c7be5e737315f0598c
didar <deddarnawzad@gmail.com>
2024-03-31 22:25:05 +0300

    config axios with baseurl and headers

---
+++ src/lib/axios.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://api.themoviedb.org/3/movie/",
+  headers: {
+      "Content-Type": "application/json",
+      "Authorization" :`Bearer ${process.env.AUTH_TOKEN}`
+  },
+});
+
+// Alter defaults after api has been created
+
+// api.interceptors.request.use(
+//   (config) => {
+//     config.headers = {
+//       // Add your custom headers here
+//       "Content-Type": "application/json",
+//       Authorization: `Bearer ${process.env.AUTH_TOKEN}`, // Example: Authorization header with token from localStorage
+//       // Add other headers as needed
+//     };
+//     return config;
+//   },
+//   (error) => {
+//     return Promise.reject(error);
+//   }
+// );
+
+export default api;

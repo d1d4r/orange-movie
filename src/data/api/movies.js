209e3097a29e5b15bc342f0204fd82838891d99c
didar <deddarnawzad@gmail.com>
2024-04-01 23:04:20 +0300

    write methode to get movies dynamicaly

--- src/data/api/movies.js
+++ src/data/api/movies.js
@@ -1,22 +1,28 @@
 import api from "@/lib/axios";
 
 export const getPopularMovies = async () => {
-  const res = await api.get("popular?language=en-US");
+  const res = await api.get("movie/popular?language=en-US");
   return res.data.results;
 };
 export const getTopRatedMovies = async () => {
-  const res = await api.get("top_rated?language=en-US");
+  const res = await api.get("movie/top_rated?language=en-US");
   return res.data.results;
 };
 export const getUpComingMovies = async () => {
-  const res = await api.get("upcoming?language=en-US");
+  const res = await api.get("movie/upcoming?language=en-US");
   return res.data.results;
 };
 export const getNowPlayMovies = async () => {
-  const res = await api.get("now_playing?language=en-US");
+  const res = await api.get("movie/now_playing?language=en-US");
   return res.data.results;
 };
 export const getMovieDetails = async (movie_id) => {
-  const res = await api.get(`${movie_id}`);
+  const res = await api.get(`movie/${movie_id}`);
   return res.data;
+};
+
+export const getDynamicMovies = async (segment) => {
+  console.log("🚀 ~ getDynamicMovies ~ segment:", segment);
+  const res = await api.get(`movie/${segment}?page=2`);
+  return { data: res.data.results, totalPages: res.data.total_pages };
 };

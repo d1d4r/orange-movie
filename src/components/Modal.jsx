56aa5a0dbb9f74a5a036dc67fa377256f84787f0
didar <deddarnawzad@gmail.com>
2024-04-03 18:48:37 +0300

    add modal to show trailer

---
+++ src/components/Modal.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { CircleX } from "lucide-react";
+import { getTrailerMovie } from "@/data/api/movies";
+
+export default async function Modal({ id }) {
+    
+    const key = await getTrailerMovie(id);
+  return (
+    <dialog
+      id="modal"
+      className="overflow-hidden size-fit backdrop:bg-black backdrop:opacity-5 "
+    >
+      <div className="flex flex-col w-full h-full">
+        <div className="self-end modal-action">
+          <form method="dialog">
+            <button className="m-2 ">
+              <CircleX />
+            </button>
+          </form>
+        </div>
+        <iframe
+          className="size-[30rem] "
+          src={`https://www.youtube.com/embed/${key}`}
+          title={"trailae"}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
+      </div>
+    </dialog>
+  );
+}

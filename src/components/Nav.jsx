81ddc859d1346f67e976d89137f3900b940ff6a3
didar <deddarnawzad@gmail.com>
2024-04-03 18:48:08 +0300

    refactor nav component with dropdown

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -9,10 +9,18 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "./ui/sheet";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
 
 export default function Nav() {
   return (
-    <header className="flex p-2 border">
+    <header className="flex p-5 border">
       <nav
         className="max-w-[85rem] hidden w-full mx-auto px-4 sm:flex flex-wrap basis-full items-center justify-between"
         aria-label="Global"
@@ -28,10 +36,10 @@ export default function Nav() {
           className="hidden overflow-hidden transition-all duration-300 hs-collapse basis-full grow sm:grow-0 sm:basis-auto sm:block sm:order-2"
         >
           <div className="flex flex-col gap-5 mt-5 sm:flex-row sm:items-center sm:mt-0 sm:ps-5">
-            <NavLink href={"#"} name={"home"} />
-            <NavLink href={"/actors"} name={"actors"} />
-            <NavLink href={"#"} name={"movies"} />
-            <NavLink href={"#"} name={"tvshow"} />
+            <MenuItemDropdown />
+            <NavLink href={"/"} name={"Home"} />
+            <NavLink href={"/actors"} name={"Actors"} />
+            <NavLink href={"/tv-show"} name={"Tvshow"} />
           </div>
         </div>
       </nav>
@@ -42,10 +50,10 @@ export default function Nav() {
         <SheetContent>
           <SheetHeader>
             <SheetDescription className="flex flex-col gap-5 mt-5 sm:items-center sm:mt-0 sm:ps-5">
-              <NavLink href={"#"} name={"home"} />
+              <MenuItemDropdown />
+              <NavLink href={"/"} name={"home"} />
               <NavLink href={"/actors"} name={"actors"} />
-              <NavLink href={"#"} name={"movies"} />
-              <NavLink href={"#"} name={"tvshow"} />
+              <NavLink href={"/tv-show"} name={"tvshow"} />
             </SheetDescription>
           </SheetHeader>
         </SheetContent>
@@ -57,3 +65,27 @@ export default function Nav() {
 export function NavLink({ href, name }) {
   return <Link href={href}>{name}</Link>;
 }
+
+export const MenuItemDropdown = () => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>Movies</DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>list</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <Link href={"/movies?type=popular"}>popular</Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Link href={"/movies?type=now_playing"}>now playing </Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Link href={"/movies?type=top_rated"}>top rated</Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Link href={"#"}>up coming </Link>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};

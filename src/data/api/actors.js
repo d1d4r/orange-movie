692950a6b2a604d73c4b6378096d3f1be42bb5da
didar <deddarnawzad@gmail.com>
2024-04-03 18:47:35 +0300

    get actor movies

--- src/data/api/actors.js
+++ src/data/api/actors.js
@@ -1,6 +1,10 @@
 import api from "@/lib/axios";
 
-export async function getAllActors() {
-  const res = await api.get(`person/popular?language=en-US&`);
+export async function getAllActors(page = 1) {
+  const res = await api.get(`person/popular?page=${page}`);
   return res.data.results;
+}
+export async function ActorHasMovie(id) {
+  const res = await api.get(`person/${id}/movie_credits`);
+  return res.data;
 }

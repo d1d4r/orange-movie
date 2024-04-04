6dce1699d34edc680db3cc2dc88c930e150da466
didar <deddarnawzad@gmail.com>
2024-04-04 23:13:15 +0300

    change api call and searchParams objec

--- src/app/movies/page.jsx
+++ src/app/movies/page.jsx
@@ -1,16 +1,28 @@
 import MoviesList from "@/components/movies/MoviesList";
-import { getAllMovies, getDynamicMovies } from "@/data/api/movies";
+import {
+  discoverMovies,
+  getAllMovies,
+  getDynamicMovies,
+} from "@/data/api/movies";
 import api from "@/lib/axios";
 import React from "react";
 
 export default async function page({ searchParams }) {
-  let { type, page } = searchParams;
+  //movie?page=500&sort_by=popularity.desc&vote_average.gte=6&with_genres=28&year=2000
+  let { sort_by, with_genres, year, page, type } = searchParams;
+  
+  console.log("🚀 ~ page ~ searchParams:", searchParams);
+  // if (sort_by || vote_average || with_genres || year || page || type isNaN(page)) {
+  //   type = "top_rated";
+  //   page = 1;
+  // }
+  const { data, totalPages } = await discoverMovies({
+    sort_by,
+    vote_average: 0,
+    with_genres,
+    year,
+    page,
+  });
 
-  if (!type && isNaN(page)) {
-    type = "top_rated";
-    page = 1;
-  }
-  const { data, totalPages = 500 } = await getDynamicMovies({ type, page });
-
-  return <MoviesList movies={data} totalPages={totalPages} type={type} />;
+  return <MoviesList movies={data} totalPages={totalPages} />;
 }

ca76e368b118f5c3084cd7c5b65c39abafb4d117
didar <deddarnawzad@gmail.com>
2024-04-07 22:08:02 +0300

    catch error and render if data is null

--- src/app/movies/page.jsx
+++ src/app/movies/page.jsx
@@ -6,23 +6,26 @@ import {
 } from "@/data/api/movies";
 import api from "@/lib/axios";
 import React from "react";
-
 export default async function page({ searchParams }) {
-  //movie?page=500&sort_by=popularity.desc&vote_average.gte=6&with_genres=28&year=2000
   let { sort_by, with_genres, year, page, type } = searchParams;
-  
+
   console.log("🚀 ~ page ~ searchParams:", searchParams);
   // if (sort_by || vote_average || with_genres || year || page || type isNaN(page)) {
   //   type = "top_rated";
   //   page = 1;
   // }
-  const { data, totalPages } = await discoverMovies({
+  const { data, totalPages, error } = await discoverMovies({
     sort_by,
     vote_average: 0,
     with_genres,
     year,
     page,
   });
+  console.log("errrrrrrrrrrrrrrrrrrrrrro", typeof error);
 
-  return <MoviesList movies={data} totalPages={totalPages} />;
+  return (
+    data ? <MoviesList movies={data} totalPages={totalPages} /> : (
+      <p>{JSON.stringify(error)}</p>
+    )
+  );
 }

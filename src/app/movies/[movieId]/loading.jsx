01fb964b2a4844fdea6b7ecf8cca08157ac7b575
didar <deddarnawzad@gmail.com>
2024-04-09 23:12:08 +0300

    delete loading

--- src/app/movies/[movieId]/loading.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function loading() {
-  return (
-    <div className="flex items-center justify-center min-h-screen">
-      loading...
-    </div>
-  );
-}

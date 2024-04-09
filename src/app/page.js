91fb4cbc3e6bcbcf81b53b5806d105916571a63e
didar <deddarnawzad@gmail.com>
2024-04-10 02:46:58 +0300

    wrape compenet in suspense

--- src/app/page.js
+++ src/app/page.js
@@ -3,21 +3,39 @@ import NowPlaying from "@/components/moviesList/NowPlaying";
 import PopularMovies from "@/components/moviesList/PopularMovies";
 import TopRated from "@/components/moviesList/TopRated";
 import UpComing from "@/components/moviesList/UpComing";
+import SwiperSkeleton from "@/components/SwiperSkeleton";
+import Link from "next/link";
+import { Suspense } from "react";
 
+const person = {
+  name: "didar",
+  age: 24,
+};
 export default async function Home() {
   return (
     <div>
       <MovieCarousel />
-     
+      {/* <Link
+        href={{
+          pathname: "/dashbord",
+          query: { person: JSON.stringify(person) },
+        }}
+      >
+        dashbord
+      </Link> */}
       <main className="space-y-10">
+        <Suspense fallback={<SwiperSkeleton />}>
+          <NowPlaying />
+        </Suspense>
+        <Suspense fallback={<SwiperSkeleton />}>
+          <TopRated />
+        </Suspense>
+
         <PopularMovies />
-        <NowPlaying />
-        <TopRated />
-        {/* 
-        <UpComing /> 
-        
-        */}
+        <UpComing />
       </main>
     </div>
   );
 }
+
+//4IsxHZfbQpe0iawGp50NwpsYE

0c61c9ae435ce3ca3830ec763fd44702bdb59e33
didar <deddarnawzad@gmail.com>
2024-04-12 22:22:35 +0300

    remove fetch movie from page and wrape movieList in suspense

--- src/app/movies/page.jsx
+++ src/app/movies/page.jsx
@@ -7,26 +7,30 @@ import {
 import api from "@/lib/axios";
 import React, { Suspense } from "react";
 export default async function page({ searchParams }) {
-
   let { sort_by, with_genres, year, page, type } = searchParams;
 
-  console.log("🚀 ~ page ~ searchParams:", searchParams);
+  //console.log("🚀 ~ page ~ searchParams:", searchParams);
   // if (sort_by || vote_average || with_genres || year || page || type isNaN(page)) {
   //   type = "top_rated";
   //   page = 1;
   // }
 
-  const { data, totalPages, error } = await discoverMovies({
-    sort_by,
-    vote_average: 0,
-    with_genres,
-    year,
-    page,
-  });
+  // const { data, totalPages, error } = await discoverMovies({
+  //   sort_by,
+  //   vote_average: 0,
+  //   with_genres,
+  //   year,
+  //   page,
+  // });
 
-  return data ? (
-    <MoviesList movies={data} totalPages={totalPages} />
-  ) : (
-    <p>{JSON.stringify(error)}</p>
+  return (
+    <Suspense fallback={<div>loading...</div>}>
+      <MoviesList
+        sort_by={sort_by}
+        with_genres={with_genres}
+        year={year}
+        page={page}
+      />
+    </Suspense>
   );
 }

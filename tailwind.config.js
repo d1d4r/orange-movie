e33d9422a7654febde038ed5f3c739c30993c763
didar <deddarnawzad@gmail.com>
2024-04-12 23:40:55 +0300

    add shade keyfreames to animate Image component

--- tailwind.config.js
+++ tailwind.config.js
@@ -67,10 +67,15 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        shade: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        shade: "shade 0.6s",
       },
     },
   },

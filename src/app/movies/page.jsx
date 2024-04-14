9cae32e6816c8d8da98433d30a1074b18a91a18a
didar <deddarnawzad@gmail.com>
2024-04-14 20:47:47 +0300

    remove unnesssery code and add uuid package to prevent caching

--- src/app/movies/page.jsx
+++ src/app/movies/page.jsx
@@ -9,26 +9,16 @@ import {
 import api from "@/lib/axios";
 import React, { Suspense } from "react";
 
+import { v4 as uuid } from 'uuid';
+
+
 export default async function page({ searchParams }) {
   let { sort_by, with_genres, year, page, type } = searchParams;
 
-  //console.log("🚀 ~ page ~ searchParams:", searchParams);
-  // if (sort_by || vote_average || with_genres || year || page || type isNaN(page)) {
-  //   type = "top_rated";
-  //   page = 1;
-  // }
-
-  // const { data, totalPages, error } = await discoverMovies({
-  //   sort_by,
-  //   vote_average: 0,
-  //   with_genres,
-  //   year,
-  //   page,
-  // });
 
   return (
-    <div key={Math.random()}>
-      {/* <Skeleton/> */}
+    <div key={uuid()}>
+
       <Filters />
       <p className="px-5 text-4xl">MOVIES</p>
       <Suspense fallback={<Skeleton />}>

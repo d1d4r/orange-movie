992cc5521c0beb46b0355a510d048af3d0a44700
didar <deddarnawzad@gmail.com>
2024-04-15 23:22:37 +0300

    filter movies by years, and sync select with url query

--- src/components/filterMovies/YearsFilter.jsx
+++ src/components/filterMovies/YearsFilter.jsx
@@ -1,28 +1,80 @@
-import React from "react";
-import Combobox from "../Combobox";
+"use client";
 
-export default function YearsFilter() {
-  const items = [
-    {
-      value: "next.js",
-      label: "1",
-    },
-    {
-      value: "sveltekit",
-      label: "2",
-    },
-    {
-      value: "nuxt.js",
-      label: "3",
-    },
-    {
-      value: "remix",
-      label: "4",
-    },
-    {
-      value: "astro",
-      label: "5",
-    },
-  ];
-  return <Combobox items={items} name="Years" />;
+import { Check, ArrowDownUp } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+} from "@/components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { useState } from "react";
+import Link from "next/link";
+import { usePathname, useSearchParams } from "next/navigation";
+
+export default function YearsFilter({ data, name }) {
+  const [open, setOpen] = useState(false);
+
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const value1 = searchParams.get("year");
+
+  const createPageURL = (years) => {
+    const params = new URLSearchParams(searchParams);
+    params.set("years", years);
+    return `${pathname}?${params.toString()}`;
+  };
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className="w-[200px] justify-between"
+        >
+          {value1
+            ? data?.find((item) => item.value === value1)?.label
+            : `Select ${name}`}
+          <ArrowDownUp className="w-4 h-4 ml-2 opacity-50 shrink-0" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0">
+        <Command>
+          <CommandInput placeholder="Search item..." className="h-9" />
+          <CommandEmpty>{`No ${name} found.`}</CommandEmpty>
+          <CommandGroup className="overflow-auto h-80">
+            {data?.map((item) => (
+              <CommandItem key={item.value} className="p-0 ">
+                <Link
+                  className="flex w-full p-2 text-center "
+                  href={createPageURL(item.value)}
+                  onClick={() => {
+                    setOpen(false);
+                  }}
+                >
+                  {item.label}
+                  <Check
+                    className={cn(
+                      "ml-auto h-4 w-4",
+                      value1 === item.value ? "opacity-100" : "opacity-0"
+                    )}
+                  />
+                </Link>
+              </CommandItem>
+            ))}
+          </CommandGroup>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
 }

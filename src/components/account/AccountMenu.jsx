324ae83cdfa56cd5b59527e40a0868e48d9d1dd7
didar <deddarnawzad@gmail.com>
2024-04-16 21:41:14 +0300

    create dropdownmenu for user account

---
+++ src/components/account/AccountMenu.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import Link from "next/link";
+
+export default function AccountMenu() {
+  return (
+    <DropdownMenu >
+      <DropdownMenuTrigger>
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="ml-4">
+        {/* <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator /> */}
+        <DropdownMenuItem asChild><Link href={'/auth/sign-up'}>Sign Up</Link></DropdownMenuItem>
+        {/* <DropdownMenuItem>Billing</DropdownMenuItem>
+        <DropdownMenuItem>Team</DropdownMenuItem>
+        <DropdownMenuItem>Subscription</DropdownMenuItem> */}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}

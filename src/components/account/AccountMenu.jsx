6b7dfaa4f0ae226b5a64d0e8b859d996ba694527
didar <deddarnawzad@gmail.com>
2024-04-18 17:23:34 +0300

    refactor AccountMenu create link to change route navigations

--- src/components/account/AccountMenu.jsx
+++ src/components/account/AccountMenu.jsx
@@ -8,21 +8,42 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "../ui/dropdown-menu";
+import OpenModal from "../OpenModal";
 import Link from "next/link";
+import { Button } from "../ui/button";
+import { CircleUserRound, LogOut } from "lucide-react";
 
 export default function AccountMenu() {
   return (
-    <DropdownMenu >
+    <DropdownMenu>
       <DropdownMenuTrigger>
         <Avatar>
           <AvatarImage src="https://github.com/shadcn.png" />
           <AvatarFallback>CN</AvatarFallback>
         </Avatar>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="ml-4">
-        {/* <DropdownMenuLabel>My Account</DropdownMenuLabel>
-        <DropdownMenuSeparator /> */}
-        <DropdownMenuItem asChild><Link href={'/auth/sign-up'}>Sign Up</Link></DropdownMenuItem>
+      <DropdownMenuContent className="ml-4 ">
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem asChild>
+          <Link href={"/account"}  className="flex justify-between w-full p-0 m-0">
+            <span>Account</span>
+            <CircleUserRound className="text-primary size-6"/>
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem asChild>
+          <Button variant="destructive" className="flex justify-between w-full p-0 m-0">
+            <span>Sign Out</span>
+            <LogOut className=" size-6" />
+          </Button>
+        </DropdownMenuItem>
+        {/* <DropdownMenuItem asChild>
+          <Link href={"/sign-up"}>SignUp</Link>
+        </DropdownMenuItem> */}
+
+        {/* <DropdownMenuItem asChild>
+          <OpenModal />
+        </DropdownMenuItem> */}
         {/* <DropdownMenuItem>Billing</DropdownMenuItem>
         <DropdownMenuItem>Team</DropdownMenuItem>
         <DropdownMenuItem>Subscription</DropdownMenuItem> */}

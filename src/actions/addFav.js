4123b728dab3db4e90e3f6098fa3b861b963141e
didar <deddarnawzad@gmail.com>
2024-04-19 22:40:34 +0300

    change media_id from static to dynamic

--- src/actions/addFav.js
+++ src/actions/addFav.js
@@ -1,11 +1,14 @@
 "use server";
 
 import api from "@/lib/axios";
+import { revalidatePath, revalidateTag } from "next/cache";
 
-export async function addFav(formData) {
+export async function addFav(movieId) {
   await api.post("account/21156670/favorite", {
     media_type: "movie",
-    media_id: 234,
+    media_id: movieId,
     favorite: true,
   });
+  revalidatePath(`/movies`);
+ // revalidateTag(tag)
 }

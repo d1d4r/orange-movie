3f156a6f3a47740ee16c648feae40c29a63da055
didar <deddarnawzad@gmail.com>
2024-04-20 22:43:52 +0300

    remove actors props, and fetch api directly

--- src/components/actors/ActorList.jsx
+++ src/components/actors/ActorList.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import ActorCard from "./ActorCard";
 import ActorPaginatio from "./ActorPaginatio";
+import { getAllActors } from "@/data/api/actors";
 
-export default function ActorList({ actors }) {
+export default async function ActorList({ searchParams }) {
+  const { page } = searchParams;
+  const actors = await getAllActors(page);
   return (
     <div>
       <div className="grid grid-cols-2 gap-2 md:grid-cols-3 xl:grid-cols-5 justify-items-center p-5">

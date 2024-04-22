0492baf891b10f5a12bffac5a2bbfbc22b3ad554
didar <deddarnawzad@gmail.com>
2024-04-22 20:15:50 +0300

    change page route to dinamic rendreing

--- src/components/account/FavMovies.jsx
+++ src/components/account/FavMovies.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import SwiperMovies from "../movies/SwiperMovies";
 import { GetFavMovies } from "@/data/api/account";
-
+export const dynamic = "force-dynamic";
 export default async function FavMovies() {
   const fav = await GetFavMovies();
 

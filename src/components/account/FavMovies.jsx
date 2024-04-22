bafc9d54b4bc3fd6eff455cfa287a5626b4356e5
didar <deddarnawzad@gmail.com>
2024-04-22 20:38:14 +0300

    remove force-dynamic segment, add noStore

--- src/components/account/FavMovies.jsx
+++ src/components/account/FavMovies.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import SwiperMovies from "../movies/SwiperMovies";
 import { GetFavMovies } from "@/data/api/account";
-export const dynamic = "force-dynamic";
+import { unstable_noStore as noStore } from 'next/cache';
+
 export default async function FavMovies() {
+  noStore();
   const fav = await GetFavMovies();
 
   return <SwiperMovies data={fav} title="favorite" href="#" />;

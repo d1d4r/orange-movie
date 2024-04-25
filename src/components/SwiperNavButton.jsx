ed0a078d31724509ce2a9e44ddd308209a8d46cc
didar <deddarnawzad@gmail.com>
2024-04-25 22:06:43 +0300

    refactor SwiperNavButtons component

--- src/components/SwiperNavButton.jsx
+++ src/components/SwiperNavButton.jsx
@@ -1,22 +1,16 @@
+'use client'
 import { ArrowLeft, ArrowRight } from "lucide-react";
-import React from "react";
-import { useSwiper } from "swiper/react";
 
-export const SwiperNavButtons = () => {
-  const swiper = useSwiper();
+export const SwiperNavButtons = ({ onClickNext, onClickPrev }) => {
 
   return (
-    <>
-      <div className="w-[40px] flex items-center justify-center h-[50%] bg-gradient-to-r from-black z-20 text-white absolute top-[10%] left-0">
-        <button onClick={() => swiper.slidePrev()}>
-          <ArrowLeft className="size-10" />
-        </button>
-      </div>
-      <div className="w-[40px] flex items-center justify-center h-[50%] bg-gradient-to-l from-black z-20 text-white absolute top-[10%] right-0">
-        <button onClick={() => swiper.slideNext()}>
-          <ArrowRight className=" size-10" />
-        </button>
-      </div>
-    </>
+    <div className="flex gap-2">
+      <button onClick={onClickPrev}>
+        <ArrowLeft className="rounded-full size-5 bg-foreground/20" />
+      </button>
+      <button onClick={onClickNext}>
+        <ArrowRight className="rounded-full size-5 bg-foreground/20" />
+      </button>
+    </div>
   );
 };

f6f59c5414db385e04f86330365cc21e35117f2e
didar <deddarnawzad@gmail.com>
2024-04-25 22:07:43 +0300

    remove comment

--- src/components/movies/MovieCard.jsx
+++ src/components/movies/MovieCard.jsx
@@ -7,21 +7,14 @@ import { getYear, roundToOneDecimalPlace } from "@/lib/formater";
 import { getGenreById } from "@/data/api/movies";
 import { Vote } from "lucide-react";
 
-export default async function MovieCard({ movie }) {
+export default async function MovieCard({ movie, path = "movies" }) {
   return (
     <div>
       <Link
-        className="flex flex-col transition-all h-96 hover:scale-90"
-        href={`/movies/${movie.id}`}
+        className="flex flex-col transition-all h-96 sm:hover:scale-90"
+        href={`/${path}/${movie.id}`}
       >
         <div className="bg-gray-100 w-fit">
-          {/* <Image
-            className="object-cover  select-none h-[18rem] animate-shade"
-            src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
-            width={300}
-            height={300}
-            alt="alt"
-          /> */}
           <ImageFallback
             className="object-cover  select-none h-[18rem] animate-shade"
             src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
@@ -32,10 +25,15 @@ export default async function MovieCard({ movie }) {
         </div>
         <div className="z-10">
           <h3 className="text-lg font-bold text-gray-800 dark:text-white line-clamp-1">
-            {movie.original_title}
+            {path === "movies" ? movie.title : movie.name}
           </h3>
           <div className="flex mt-2 text-gray-500 dark:text-gray-400">
-            <p className="">{getYear(movie.release_date)}|</p>
+            <p className="">
+              {path === "movies"
+                ? getYear(movie.release_date)
+                : getYear(movie.first_air_date)}
+              |
+            </p>
             <div className="flex">
               <Vote />
               <p>{roundToOneDecimalPlace(movie.vote_average)}</p>
@@ -43,7 +41,7 @@ export default async function MovieCard({ movie }) {
           </div>
         </div>
       </Link>
-      {/* <FavButton movieId={movie.id} /> */}
+      {/* <FavButton movieId={movie.id} /> first_air_date */}
     </div>
   );
 }

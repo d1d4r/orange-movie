c64343953395159a2a9f745020ae75333049c57f
didar <deddarnawzad@gmail.com>
2024-04-25 22:06:19 +0300

    improve SearchCommand fanctionality

--- src/components/SearchCommand.jsx
+++ src/components/SearchCommand.jsx
@@ -1,25 +1,32 @@
 "use client";
-
 import { SearchMulti } from "@/data/api/movies";
 import { Input } from "./ui/input";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "./ui/tabs";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SearchCard from "./movies/SearchCard";
+import { usePathname } from "next/navigation";
 
 export function SearchCommand() {
   const [SearchResult, setSearchResult] = useState({ movie: [], tv: [] });
+  const pathname = usePathname();
 
   const SearchHandle = async (e) => {
     const { movie, tv } = await SearchMulti(e.target.value);
     setSearchResult((prev) => ({ ...prev, movie: movie, tv: tv }));
   };
 
+  useEffect(() => {
+    if (pathname !== "/") {
+      setSearchResult({ movie: [], tv: [] });
+    }
+  }, [pathname]);
+
   const closeSearch = () => {
     console.log("clicked");
   };
 
   return (
-    <div className="flex w-full max-w-sm items-center space-x-2 relative">
+    <div className="flex items-center w-full max-w-sm space-x-2 ">
       <Input
         className="rounded-lg"
         placeholder="Search..."
@@ -28,9 +35,9 @@ export function SearchCommand() {
       />
 
       {(SearchResult.movie.length > 0 || SearchResult.tv.length > 0) && (
-        <div className="bg-white border  rounded-md  w-[600px] absolute top-12 left-0 right-0 z-30 ">
+        <div className="absolute right-0 z-30 h-screen w-screen bg-white border rounded-md md:w-[600px] md:left-0 top-14 overflow-y-scroll overflow-x-hidden">
           <div className="">
-            <Tabs defaultValue="movies" className="w-[400px]">
+            <Tabs defaultValue="movies" className="">
               <div>
                 <TabsList>
                   <TabsTrigger value="movies">
@@ -58,6 +65,7 @@ export function SearchCommand() {
                     {SearchResult.tv.map((movie) => {
                       return (
                         <SearchCard
+                          path="tv-show"
                           key={movie.id}
                           movie={movie}
                           onClick={closeSearch}

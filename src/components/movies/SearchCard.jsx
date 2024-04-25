ecc763e6284a4b0643c39f4ab2076e54e52183e3
didar <deddarnawzad@gmail.com>
2024-04-25 22:05:06 +0300

    refactore SearchCard

--- src/components/movies/SearchCard.jsx
+++ src/components/movies/SearchCard.jsx
@@ -1,47 +1,33 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import { getYear, roundToOneDecimalPlace } from "@/lib/formater";
-import { Vote } from "lucide-react";
+import { getYear } from "@/lib/formater";
 import ImageFallback from "./ImageFallback";
 
-export default function SearchCard({ movie }) {
+export default function SearchCard({ movie, path = "movies" }) {
   return (
     <Link
-      className="flex flex-row transition-all w-[600px] gap-4 borde hover:bg-foreground/10  p-2 "
-      href={`/movies/${movie.id}`}
+      className="w-full transition-all border hover:bg-foreground/10"
+      href={`/${path}/${movie.id}`}
     >
-      {/* <Image
-        className="object-cover select-none w-[12rem] h-[6rem] animate-shade rounded-md"
-        src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
-        width={192}
-        height={96}
-        alt="alt"
-      /> */}
-      <ImageFallback
-        className="object-cover select-none w-[12rem] h-[6rem] animate-shade rounded-md"
-        src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
-        width={192}
-        height={96}
-        alt="alt"
-      />
-
-      <div className="z-10 ">
-        <h3 className="text-lg font-bold text-gray-800 dark:text-white line-clamp-1">
-          {movie.original_title} | {getYear(movie.release_date)}
-          {/* panda */}
-        </h3>
-        <div className="flex flex-col text-gray-500 dark:text-gray-400">
-          {/* <p className="">
-            2022
-            {getYear(movie.release_date)}
-          </p> */}
-          <p className="line-clamp-3">
-            {/* Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
-            repellendus tempora assumenda eligendi ipsam et placeat quia rem
-            explicabo maxime. */}
-            {movie.overview}
-          </p>
+      <div className="grid grid-cols-3 grid-rows-1 gap-2">
+        <div className="col-start-1 place-self-center row-start-1 h-[5rem] bg-foreground/80">
+          <ImageFallback
+            className="object-cover  select-none w-[8rem] h-full animate-shade rounded-md"
+            src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+            width={128}
+            height={80}
+            alt="alt"
+          />
+        </div>
+        <div className="col-span-2 row-start-1">
+          <h3 className="text-lg font-bold text-gray-800 dark:text-white line-clamp-1">
+            {path === "movies" ? movie.title : movie.name} |
+            {path === "movies"
+              ? getYear(movie.release_date)
+              : getYear(movie.first_air_date)}
+          </h3>
+          <p className="line-clamp-3 ">{movie.overview}</p>
         </div>
       </div>
     </Link>

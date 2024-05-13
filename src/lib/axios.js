f2249fbb4524b9f2869523e94cba6d07f27a71d1
didar <deddarnawzad@gmail.com>
2024-05-13 22:54:51 +0300

    change NEXT_PUBLIC_AUTH_TOKEN to NEXT_PUBLIC_ACCESS_TOKEN_AUTH

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -3,10 +3,9 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "https://api.themoviedb.org/3/",
   headers: {
-      "Content-Type": "application/json",
-      "Authorization" :`Bearer ${process.env.NEXT_PUBLIC_AUTH_TOKEN}`
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_ACCESS_TOKEN_AUTH}`,
   },
 });
-
 
 export default api;
